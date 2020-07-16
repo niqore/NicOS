3be7ebb466211ec8d3ff448550b85ae1d5275de7
Nicolas Virard <nicolas.virard@ecole.ensicaen.fr>
2020-07-16 19:20:56 +0200

    Fat32 format

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -1,6 +1,25 @@
 [org 0x7c00] ; Indique à l'assembleur que l'offset est celui du bootsector. Il va donc décaler toutes données du code par 0x7c00
 
-jmp 0x00:start ; On met cs à 0
+;jmp 0x00:start ; On met cs à 0
+;nop
+
+; FAT 32 header
+db 0xEB,0x58,0x90
+db 'nicosfat'
+db 0x00,0x02,0x01,0x20,0x00,0x02
+times 4 db 0
+db 0xF8,0x00,0x00,0x20,0x00,0x40
+times 5 db 0
+db 0x40,0x0D,0x03,0x00,0x03,0x06
+times 6 db 0
+db 0x02
+times 3 db 0
+db 0x01,0x00,0x06
+times 13 db 0
+db 0x80,0x00,0x29,0xA2,0x0E,0x7F,0x8C
+db 'NO NAME    '
+db 'FAT32   '
+
 
 start:
   xor ax, ax ; On met ax à 0
@@ -13,15 +32,6 @@ KERNEL_OFFSET equ 0x1000 ; Définition de l'offset du kernel
 mov [BOOT_DRIVE], dl ; Le BIOS enregistre le périphérique de démarrage dans dl donc on l'enregistre pour l'utiliser plus tard si besoin
 mov bp, 0x7c00 ; pile
 mov sp, bp
-
-mov bx, WELCOME
-call print
-call print_nl
-call print_nl
-
-mov bx, PROT_MODE_LOAD_MSG
-call print
-call print_nl
 
 call load_kernel
 
@@ -36,24 +46,11 @@ jmp $
 
 [bits 16]
 load_kernel:
-	mov bx, MSG_LOAD_KERNEL
-	call print
-	call print_nl
-
 	mov bx, KERNEL_OFFSET ; Lecture depuis le disque et enregistrement en 0x1000
 	mov dh, 50
 	mov dl, [BOOT_DRIVE]
 	call disk_load
 	ret
-
-WELCOME:
-	db 'Bienvenue sur NicOS !', 0; Le 0 sert à terminer la chaine de caractère
-	
-PROT_MODE_LOAD_MSG:
-	db 'Passage en mode 32-bit protected...', 0
-
-MSG_LOAD_KERNEL:
-	db 'Chargement du kernel', 0
 
 BOOT_DRIVE db 0
 
