4c9919d5ad02d742e48df74aa7e803189ef823f6
Nicolas Virard <nicolas.virard@ecole.ensicaen.fr>
2020-08-23 12:28:21 +0200

    File copyright headers

--- cpu/interrupts/idt.h
+++ cpu/interrupts/idt.h
@@ -1,3 +1,18 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin 
+ * F-14050 Caen Cedex 
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */ 
+
+/**
+ * @author Nicolas Virard <nicolas.virard@ecole.ensicaen.fr> 
+ * @version     0.0.1 - 23-08-2020
+ */
+
 #ifndef IDT_H
 #define IDT_H
 
